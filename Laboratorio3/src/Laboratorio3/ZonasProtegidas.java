6cde347caa37e81f17a88229294e9a18fdf9cef5
Satellaizar <Satellaizar@192.168.1.104>
2018-11-03 19:28:10 -0600

    Laboratorio3, Universidad De Costa Rica, Recinto Tacares

---
+++ Laboratorio3/src/Laboratorio3/ZonasProtegidas.java
@@ -0,0 +1,12 @@
+
+package Laboratorio3;
+
+/**
+ *
+ * @author Edwin Molina , Juan Carlos Valverde
+ * @version 3 De Noviembre, 2018
+ */
+public interface ZonasProtegidas {
+    
+    public int ingresosEstado ();
+}
