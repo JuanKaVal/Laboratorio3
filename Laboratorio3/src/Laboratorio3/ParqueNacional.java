9355b2d807c3c9a9b2b8d0018123e297744a0ead
Edwin David <Edwin David@192.168.1.3>
2018-11-03 22:26:58 -0600

    Metodo calcular ingresos

--- Laboratorio3/src/Laboratorio3/ParqueNacional.java
+++ Laboratorio3/src/Laboratorio3/ParqueNacional.java
@@ -1,12 +1,42 @@
 
 package Laboratorio3;
-
+   
 /**
  *
  * @author Edwin Molina , Juan Carlos Valverde
  * @version 3 De Noviembre, 2018
  */
 public class ParqueNacional extends ZonasProtegidas{
+    private int clientesElectricidad;
+    private int cantCostarricenses;
+    private int cantExtrangeros;
+
+    public ParqueNacional() {
+        this("","",0,0,0,0);
+    }
+    
+    public ParqueNacional(String nombre, String provincia, int montoIngresos, int cantCostarricenses, int cantExtrangeros, int clientesElectricidad) {
+    super(nombre, provincia, montoIngresos);
+    this.cantCostarricenses = cantCostarricenses;
+    this.cantExtrangeros = cantExtrangeros;
+    this.clientesElectricidad = clientesElectricidad;
+    }
+    
+
+    @Override
+    public int calcularIngresos() {
+        int subvencion = (1200 * clientesElectricidad);
+        int ingresosEntradas = (cantCostarricenses * 5000) + (cantExtrangeros * 6000);
+        int totalIngresos = subvencion + ingresosEntradas;
+        return totalIngresos;
+        
+    }
+    
+    
+    
     
     
 }
+
+    
+
