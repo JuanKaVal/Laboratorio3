aecf64595e6aa160b5b975eee7ab033c63a10f38
usuario <usuario@AFila4PC4>
2018-11-06 08:47:59 -0600

    Prueba sales

--- Laboratorio3/src/Laboratorio3/ReporteVector.java
+++ Laboratorio3/src/Laboratorio3/ReporteVector.java
@@ -6,23 +6,26 @@ package Laboratorio3;
  * @author Edwin Molina , Juan Carlos Valverde
  * @version 3 De Noviembre, 2018
  */
-public abstract class ReporteVector {
-
-    private String[] reports;
+public class ReporteVector {
+    
     private static final int CAP = 4;
+    ParqueNacional parque = new ParqueNacional("Madre Verde", "Alajuela",2000000 , 2000, 7000, 2500);
+    
     
     public ReporteVector() {
         String[] reports = new String[CAP];
     }
     
-    
-
     public String[] report1 () {
         String[] reports = new String[CAP];
         for (String i:reports) {
             
         }
         return null;
+    }
+    
+    public String[] report2 () {
+        
     }
     
     public void crecimiento () {
