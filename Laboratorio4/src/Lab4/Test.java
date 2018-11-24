0bc3a620ea561988b553303f1b482f06c2167880
Edwin David <Edwin David@192.168.1.3>
2018-11-23 22:17:34 -0600

    Ejemplo

--- Laboratorio4/src/Lab4/Test.java
+++ Laboratorio4/src/Lab4/Test.java
@@ -12,7 +12,7 @@ public class Test {
         int[] test = {1,2,3,4,5};
         Lab4.invert(test, 0, test.length-1);
         //Lab4.print(test);
-        
+            System.out.println(Lab4.binaryToDecimal(10, 0));
         
         int[]domin = {50000,20000,10000,5000,1000,500,100,50,25,10,5};
         System.out.println(Lab4.atm(42750, domin, 0, 0));
